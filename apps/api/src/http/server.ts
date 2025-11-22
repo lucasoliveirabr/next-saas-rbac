8781eaf80048bd2c79d320f4147f54ba0a6afd61
lucasoliveirabr <lucas.oliveirabr@outlook.com>
2025-11-22 16:12:41 +0000

    feat: add swagger/openapi docs with scalar ui

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,16 +1,34 @@
 import fastifyCors from "@fastify/cors";
 import fastify from "fastify";
 import {
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
 } from "fastify-type-provider-zod";
 import { createAccount } from "./routes/auth/create-account";
+import scalarFastify from "@scalar/fastify-api-reference";
+import fastifySwagger from "@fastify/swagger";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Next SaaS",
+      description: "Full-Stack SaaS app with multi-tenant & RBAC.",
+      version: "1.0.0",
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+
+app.register(scalarFastify, {
+  routePrefix: "/docs",
+});
 
 app.register(fastifyCors);
 
